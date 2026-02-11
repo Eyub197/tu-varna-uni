e9f71a22782f41e99af8078a67b670726e89335c
eyub197 <eyubmehmed122@gmail.com>
2026-02-11 12:50:17 +0200

    Tested google formating 1 file lost

--- 1-basic-programming/9-find-y.cpp
+++ 1-basic-programming/9-find-y.cpp
@@ -9,15 +9,14 @@ int main() {
   cout << "Enter x: " << endl;
   cin >> x;
 
-  if( x < 0) {
-      y = 4 * x - 7;
+  if (x < 0) {
+    y = 4 * x - 7;
   } else if (x == 0) {
-      y = 0;
+    y = 0;
   } else {
-      y = 5 + 3 * x;
+    y = 5 + 3 * x;
   }
 
   cout << "The value of x is: " << x << endl;
   cout << "The value of y is: " << y << endl;
-
 }
