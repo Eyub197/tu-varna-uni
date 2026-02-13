bf10f0079eb78bbe8c8cef3776b11d21cba0cafd
eyub197 <eyubmehmed122@gmail.com>
2026-02-13 21:15:30 +0200

    This was just a test project I will move it soon          gc

--- 1-basic-programming/24-students-manegment-with-files.cpp
+++ 1-basic-programming/24-students-manegment-with-files.cpp
@@ -4,18 +4,15 @@
 using namespace std;
 
 void writeStudentInFile() {
-    fstream inputFile("students.dat", ios::out | ios::binary);
-
+  fstream inputFile("students.dat", ios::out | ios::binary);
 }
 
 struct Student {
-    char name[15];
-    float programminGrade;
-    float mathGrade;
+  char name[15];
+  float programminGrade;
+  float mathGrade;
 };
 
 const int MAX_COUNT_OF_STUDENTS = 3;
 
-int main () {
-  Student students[MAX_COUNT_OF_STUDENTS] = {};
-}
+int main() { Student students[MAX_COUNT_OF_STUDENTS] = {}; }
