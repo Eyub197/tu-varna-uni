e4a48bf80eec0a416884dc6129f95de620fde882
Eyub Mehmed Eyub <eyubmehmed122@gmail.com>
2026-02-20 14:08:27 +0200

    test

--- 1-basic-programming/3-suming.cpp
+++ 1-basic-programming/3-suming.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 
+int Suming(int a, int b) { return a + b; }
 
-int Suming(int a, int b) {
-    return a + b;
-}
+int main() {
+  int a;
+  int b;
+  int sum;
 
-int main () {
-    int a;
-    int b;
-    int sum;
+  std::cout << "Enter first number: ";
+  std::cin >> a;
 
-    std::cout << "Enter first number: ";
-    std::cin >> a;
+  std::cout << "Enter second number: ";
+  std::cin >> b;
 
-    std::cout << "Enter second number: ";
-    std::cin >> b;
+  sum = Suming(a, b);
 
-    sum = Suming(a, b);
-
-    std::cout << "the sum: " << sum << std::endl;
+  std::cout << "the sum: " << sum << std::endl;
 }
---
+++ 1.3-algorithms-and-data-structures/1-lab-exercise.cpp
@@ -0,0 +1,3 @@
+// I forgot to push it it was some sort of a matrix and for odd rows we were
+// finding the biggest num and for even rows we were finding the smallest ones and then printing in row x the smalles number is 
+// number then in row x the biggest number is number and so on
