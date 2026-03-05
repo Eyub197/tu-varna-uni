517b5cbde17da0f8f4c5ce616b0b9670103aa1fe
Eyub197 <eyubmehmed122@gmail.com>
2026-03-05 21:26:59 +0200

    Another exercise to check if point is inside or outside

---
+++ 1.2-basic-programming-2/3-Conditionals/5.Lab-exercise.py
@@ -0,0 +1,8 @@
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
+x = float(input("Enter x: "))
+y = float(input("Enter y"))
+
+print("Inside") if x >= x1 and x <= x2 and y >= y1 and y <= y2 else print("Outside")
